f103046f028226ab203a4a4c0866cfe90c1ebf79
MyLastCard <vonwao@hotmail.com>
2015-10-19 19:51:41 -0400

    refactor components

--- audit/routes.jsx
+++ audit/routes.jsx
@@ -9,6 +9,27 @@
 //});
 
 
+function withAudit(Component) {
+  return React.createClass({
+    mixins: [ReactMeteorData],
+
+    getMeteorData() {
+      console.log('id: ' + this.props.params.slug)
+      return {
+        //item: Audits.findOne({ _id: this.props.params.slug })
+        item: Audits.findOne()
+      };
+    },
+
+    render() {
+      return <div>
+      Ok
+    <Component item={this.data.item} collection={Audits} {...this.props} />
+    </div>
+    }
+  })
+};
+
 function renderForm(section) {
   return React.createClass({
     render() {
@@ -22,10 +43,20 @@ function renderForm(section) {
 
 Reaktor.init(
   <Router>
-    <Route path="/audit" layout={Layout2} content={AuditList} />
-    <Route path="/audit/main" layout={Layout2} content={AuditInput} />
-    <Route path="/audit/MD" layout={Layout2} content={renderForm(AuditDataMDSchema)} />
-    <Route path="/audit/DC" layout={Layout2} content={renderForm(AuditDataDCSchema)} />
-    <Route path="/audit/report" layout={Layout2} content={AuditRecommendations} />
+    <Route path="/audit"
+           layout={Layout2}
+           content={AuditList} />
+    <Route path="/audit/main"
+           layout={Layout2}
+           content={AuditInput} />
+    <Route path="/audit/MD"
+           layout={Layout2}
+           content={renderForm(AuditDataMDSchema)} />
+    <Route path="/audit/DC"
+           layout={Layout2}
+           content={renderForm(AuditDataDCSchema)} />
+    <Route path="/audit/report"
+           layout={Layout2}
+           content={withAudit(AuditRecommendations)} />
   </Router>
 );

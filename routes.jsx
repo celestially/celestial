f1434138c2ecbfd04c01cbdf72cd96e33823a97b
MyLastCard <vonwao@hotmail.com>
2015-10-23 15:39:02 -0400

    more clean up

--- routes.jsx
+++ routes.jsx
@@ -1,10 +1,7 @@
 Reaktor.init(
   <Router>
     <Route path="/" layout={Layout} content={Home} />
-    <Route path="/grid" layout={Layout} content={GridDemo} />
     <Route path="/collConfig" layout={Layout} content={CollectionConfig} />
-
-    <Route path="/grid/ex1" layout={Layout} content={GridEx1} />
 
     <Route path="/audit/list" layout={Layout2} content={auditList(ItemList)}/>
     <Route path="/audit/table" layout={Layout2} content={AuditTable}/>
